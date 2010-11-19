98be00f8752d1665217c539acb3901941b577205
fen <fen@93305231-c19d-01b2-fa94-c65a495c7d90>
2010-11-19 15:39:16 +0000

    Added functionality so that a "choice" can be edited, also a language-chooser is avaible for the textboxes it should appear for

--- confolio/trunk/src/rforms/view/Editor.js
+++ confolio/trunk/src/rforms/view/Editor.js
@@ -6,6 +6,9 @@ dojo.require("dijit.form.Button");
 dojo.require("dijit.form.FilteringSelect");
 dojo.require("dojo.data.ItemFileReadStore");
 dojo.require("rforms.template._BaseItem");
+dojo.require("dijit.form.RadioButton");
+
+rforms.template.uniqueRadioButtonNameNr = 0;
 
 dojo.declare("rforms.view.Editor", rforms.view.Presenter, {
 	filterTranslations: false,
@@ -82,6 +85,27 @@ dojo.declare("rforms.view.Editor", rforms.view.Presenter, {
 		dojo.addClass(tb.domNode, "fieldInput");
 		
 		var remove = new dijit.form.Button({label: "remove"}, dojo.create("div", null, controlDiv));
+		
+		//If the language can be set
+		var item = binding.getItem();
+		var nodeType = item.getNodetype();
+		if(nodeType === "LANGUAGE_LITERAL" || nodeType === "PLAIN_LITERAL"){
+			var langSpan = dojo.create("span", null, controlDiv);
+			var langList = this._getLanguagesList();
+			var langStore = this._getItemFileReadStoreFromArray(langList,binding.getItem());
+			var languageSelector = new dijit.form.FilteringSelect({
+					store: langStore,
+					searchAttr: "label"
+				}, langSpan);
+			languageSelector.set("value", binding.getLanguage());				
+			dojo.connect(languageSelector, "onChange", dojo.hitch(this, function(){
+					binding.setLanguage(languageSelector.getValue());
+				}));
+			dojo.addClass(langSpan, "language");
+			dojo.addClass(fieldDiv,"langcontrolledfield");
+			dojo.addClass(controlDiv, "langFieldControl");
+		}
+		
 		var cardConnect1 = dojo.connect(binding.getCardinalityTracker(), "minReached", function() {
 			remove.attr("disabled", true);
 		});
@@ -89,7 +113,10 @@ dojo.declare("rforms.view.Editor", rforms.view.Presenter, {
 		var cardConnect2 = dojo.connect(binding.getCardinalityTracker(), "justFine", function() {
 			remove.attr("disabled", false);
 		});
-
+		
+		//Inactivates buttons at startup if needed
+		binding.getCardinalityTracker().checkCardinality();
+		
 		var removeConnect = dojo.connect(remove, "onClick", function() {
 			if (binding.getCardinalityTracker().getCardinality() === 1) {
 				binding.setValue(null);
@@ -101,22 +128,82 @@ dojo.declare("rforms.view.Editor", rforms.view.Presenter, {
 				binding.remove();
 				dojo.destroy(fieldDiv);
 			}
+			if (nodeType === "LANGUAGE_LITERAL" || nodeType === "PLAIN_LITERAL") {
+					languageSelector.set("value", "");
+			}
 		});
 		
 
 	},
 	addChoice: function(fieldDiv, binding) {
-		//Create an ItemFileReadStore with the correct language to use 
+		 
 		var item = binding.getItem();
-		var store = this._createChoiceStore(item);
+		var choices = item.getChoices();
+		var controlDiv = dojo.create("div", null, fieldDiv);
+		dojo.addClass(controlDiv, "fieldControl");
 		
-		//The FilteringSelect is set on a "span" that is added to the fieldDiv
-		var spanToUse = dojo.create("span",null,fieldDiv);
-		var fSelect = new dijit.form.FilteringSelect({store: store, searchAttr: "label"}, spanToUse);
-		
-		//Sets the value if any
-		if (binding.getValue()) {
-			fSelect.set("value", binding.getValue());
+		if (choices.length < 5 && item.getCardinality.max === 1) {
+			for (var ib in choices) {
+				var divToUse =  dojo.create("div", null, fieldDiv);
+				var inputToUse = dojo.create("input", null, divToUse);
+				dojo.create("span", { innerHTML: item._getLocalizedValue(choices[ib].label).value }, divToUse);
+				var rb = new dijit.form.RadioButton({
+					name: "RadioButtonName"+rforms.template.uniqueRadioButtonNameNr,
+					value: choices[ib].d,
+					checked: choices[ib].d === binding.getValue()
+				}, inputToUse);
+				dojo.connect(rb, "onClick", dojo.hitch(this, function(){
+					binding.setValue(rb.getValue());
+				}));
+			}
+			rforms.template.uniqueRadioButtonNameNr++;
+		} else {
+			//Create an ItemFileReadStore with the correct language to use
+			var store = this._createChoiceStore(item);
+			var spanToUse = dojo.create("span", null, fieldDiv);
+			var fSelect = new dijit.form.FilteringSelect({
+				store: store,
+				searchAttr: "label"
+			}, spanToUse);
+			
+			//Sets the value if any
+			if (binding.getValue()) {
+				fSelect.set("value", binding.getValue());
+			}
+			//Callback when the user edits the value
+			fSelect.onChange = dojo.hitch(this, function (newvalue) {
+					binding.setValue(newvalue);
+			});
+			
+			/*Code below is to correctly remove items in the form and their
+			 * values
+			 */
+			
+			//The button
+			var remove = new dijit.form.Button({label: "remove"}, dojo.create("span", null, fieldDiv));
+			
+			var cardConnect1 = dojo.connect(binding.getCardinalityTracker(), "minReached", function() {
+				remove.attr("disabled", true);
+			});
+
+			var cardConnect2 = dojo.connect(binding.getCardinalityTracker(), "justFine", function() {
+				remove.attr("disabled", false);
+			});
+			
+			//Inactivates buttons at startup if needed
+			binding.getCardinalityTracker().checkCardinality();
+			
+			var removeConnect = dojo.connect(remove, "onClick", function() {
+				if (binding.getCardinalityTracker().getCardinality() === 1) {
+					fSelect.set("value", "");
+				} else {
+					dojo.disconnect(cardConnect1);
+					dojo.disconnect(cardConnect2);
+					dojo.disconnect(removeConnect);
+					binding.remove();
+					dojo.destroy(fieldDiv);
+				}
+			});
 		}
 	},
 	/*
@@ -125,10 +212,21 @@ dojo.declare("rforms.view.Editor", rforms.view.Presenter, {
 	 */
 	_createChoiceStore: function(/*Choice*/ item){
 		var choices = item.getChoices();
+		return this._getItemFileReadStoreFromArray(choices, item);
+	},
+	/*
+	 * From an array of choices that contains value and labels an 
+	 * ItemFileReadStore is created and returned. The object inside 
+	 * the array should have the following structure:
+	 *  {"d": "Value",
+	 *  "label": {"en": "English-label", "sv": "Svensk label"}
+	 * }
+	 */
+	_getItemFileReadStoreFromArray: function(/*Array of objects*/objects, /*The item*/ item){
 		var itemsArray = [];
-		for (var i in choices){
-			var currentLabel = item._getLocalizedValue(choices[i].label);
-			itemsArray.push({d:choices[i].d, label:currentLabel.value});
+		for (var i in objects){
+			var currentLabel = item._getLocalizedValue(objects[i].label);
+			itemsArray.push({d:objects[i].d, label:currentLabel.value});
 		}
 		var store = dojo.data.ItemFileReadStore({
 			data: {
@@ -138,5 +236,22 @@ dojo.declare("rforms.view.Editor", rforms.view.Presenter, {
 			}
 		});
 		return store;
+	},
+	/*
+	 * 
+	 * This method returns a list of language-codes and their label (in several translations)
+	 * An example for English looks like this:
+	 * {"d": "en",
+	 *  "label": {"en": "English", "sv": "Engelska"}
+	 * }
+	 *  
+	 * @return {Array} of languages.
+	 */
+	_getLanguagesList:function (){ //TODO: Take this list from some kind of configuration
+		var list = [{"d": "", label:{"en":"", "sv":""}},
+		            {"d": "en", label:{"en":"English", "sv":"Engelska"}},
+		            {"d": "de", label:{"en":"German", "sv":"Tyska"}},
+					{"d": "sv", label:{"en":"Swedish", "sv":"Svenska"}},];
+		return list;
 	}
 });
